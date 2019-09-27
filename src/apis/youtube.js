bae78947235ce8ded53158298cb00137e0e8f29a
pmapelli <pmapelli@hotmail.com>
2019-09-27 09:21:26 -0300

    feat: Searching for videos-Putting it All Together

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "AIzaSyDWWwN7AVUwfvFVzj3v23IQmfrjgRv2kCo";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY
+  }
+});
