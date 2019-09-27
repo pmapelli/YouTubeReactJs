df305422c98b37f453f21a8fd9c8cc3556269c5a
pmapelli <pmapelli@hotmail.com>
2019-09-27 10:48:13 -0300

    feat: Rendering a List of Videos

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,7 +1,12 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
-const VideoList = props => {
-  return <div>{props.videos.length}</div>;
+const VideoList = ({ videos }) => {
+  const renderList = videos.map(video => {
+    return <VideoItem />;
+  });
+
+  return <div>{renderList}</div>;
 };
 
 export default VideoList;
